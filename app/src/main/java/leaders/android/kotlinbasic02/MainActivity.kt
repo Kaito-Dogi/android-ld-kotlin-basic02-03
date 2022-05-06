96ec50cdf03e48c9c3ab75cd3b88d063d6f1ecd7
Kaito-Dogi <kendobu0405@gmail.com>
2022-05-07 04:28:32 +0900

    feat: viewBinding

--- app/src/main/java/leaders/android/kotlinbasic02/MainActivity.kt
+++ app/src/main/java/leaders/android/kotlinbasic02/MainActivity.kt
@@ -1,13 +1,21 @@
 package leaders.android.kotlinbasic02
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import leaders.android.kotlinbasic02.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        // 変数宣言
+
+        // プラスボタンをクリックした時の処理
 
     }
 }
